427649366a32147ccb3256a0ec6fea9227b6486c
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-03-01 20:44:20 +0800

    Updated the player.py and player.uxf

--- GoW/player.py
+++ GoW/player.py
@@ -1,1 +1,31 @@
 print("try player")
+
+class Player():
+    def __init__(self, n):
+        self.name = n
+        self.playPile = new Pile()
+        self.wonPile = new Pile()
+
+    Card playCard():
+        if (playPile.getSize() == 0):
+            useWonPile()
+        if (playPile().getSize > 0):
+            return playPile.nextCard()
+        return None
+
+    def getName(self):
+        return self.name
+
+    def collectCard(self, Card c):
+        self.wonPile.addCard(c)
+
+    def collectCard(self, Pile p):
+        self.wonPile.addCard(p)
+
+    def useWonPile(self):
+        playPile.clear()
+        playPile.addCard(wonPile)
+        wonPile.clear()
+
+    def numCard(self);
+        return playPile.getSize() + wonPile.getSize()
