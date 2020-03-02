03d0b3932b6ad48d6f6799dc98d82870dc9253fb
Clint Aldrin Valencia <caav1635@gmail.com>
2020-03-02 18:44:01 +0800

    latest update for card.py

--- GoW/card.py
+++ GoW/card.py
@@ -1,13 +1,14 @@
+#clint Valencia
 class Card:
 	def __init__(self, r, s):
-		self.__rank = r
-		self.__suit = s
+		self.rank = r
+		self.suit = s
 	def getRank(self):
-		return self.__rank
+		return self.rank
 	def getSuit(self):
-		return self.__suit
+		return self.suit
 	def compareTo(self, ob):	
-		other = Card(ob.__rank, ob.__suit)	
+		other = Card(ob.rank, ob.suit)	
 		selfRank = self.getRank()
 		otherRank = other.getRank()
 		if(selfRank == 1): selfRank = 14
@@ -16,15 +17,15 @@ class Card:
 	def __eq__(self, ob):
 		if(isinstance(ob,Card)):
 			other = Card(ob.__rank, ob.__suit)
-			return self.__rank == other.__rank and self.__suit == other.__suit
+			return self.rank == other.__rank and self.suit == other.__suit
 		else:
 			return false
 	def __str__(self):
 		suitList = ["", "Clubs", "Diamonds", "Hearts", "Spades"]
-		if(self.__rank == 1): val = "Ace"
-		elif(self.__rank == 11): val = "Jack"
-		elif(self.__rank == 12): val = "Queen"
-		elif(self.__rank == 13): val = "King"
-		else: val = str(self.__rank)
-		s = val  + " of " + suitList[self.__suit]
+		if(self.rank == 1): val = "Ace"
+		elif(self.rank == 11): val = "Jack"
+		elif(self.rank == 12): val = "Queen"
+		elif(self.rank == 13): val = "King"
+		else: val = str(self.rank)
+		s = val  + " of " + suitList[self.suit]
 		return s
