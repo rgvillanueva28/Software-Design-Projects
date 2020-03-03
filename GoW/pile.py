be024515897bdcf5efa98cef1ad8fba5cbf4986e
Bob Laurence Caridad <boblcaridad@gmail.com>
2020-03-04 00:03:44 +0800

    modifications for pile.py and pile.uxf from me

--- GoW/pile.py
+++ GoW/pile.py
@@ -1,37 +1,35 @@
 #Bob Caridad
 
 class Pile:
-    def _init_(self, r, s):
-        self._rank = r
-        self._suit = s
 
-    def getRank(self):
-        return self._rank
-    
-    def getSuit(self):
-        return self._suit
-    
-    def compareTo(self, ob):
-        other = Pile(ob._rank, ob._suit)
-        selfRank = self.getRank()
-        otherRank = other.getRank()
-        if(selfRank == 1): selfRank = 14
-        if(otherRank == 1): otherRank = 14
-        return selfRank - otherRank
+	def __init__(self):
+		self.__pile = [None] * 52
+		self.__front = 0
+		self.__end = 0
 
-    def _eq_(self, ob):
-        if(isinstance(ob,Pile)):
-            other = Pile(ob._rank, ob._suit)
-            return self._rank == other._rank and self._suit == other._suit
-        else:
-            return false
+	def getSize(self):
+		return self.__end - self.__front
+        
+	def clear(self):
+		self.__front = 0
+		self.__end = 0
 
-    def _str_(self):
-        suitList = ["", "Clubs", "Diamonds", "Hearts", "Spades"]
-        if(self._ranl == 1): val = "Ace"
-        elif(self._rank == 11): val = "Jack"
-        elif(self._rank == 12): val = "Queen"
-        elif(self._rank == 13): val = "King"
-        else: val = str(self._rank)
-        s = val + " of " + suitList[self._suit]
-        return s
+	def addCard(self, c):
+		self.__pile[self.__end] = c
+		self.__end += 1
+		return c
+
+	def addCards(self, p):
+		while(p.getSize() > 0):
+			self.addCard(p.nextCard())
+
+	def nextCard(self):
+		if(self.__front == self.__end):
+			return None
+		c = self.__pile[self.__front]
+		self.__front += 1
+		return c
+	
+	def display(self):
+		for i in range(0, self.__end):
+			print(self.__pile[i], end = " ")
