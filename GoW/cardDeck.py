80f828d02a8010982de63d0a10f87936b1ef52ff
Gian Karlo R. Madrid <madridkarlo7@gmail.com>
2020-03-03 22:35:45 +0800

    Updated CardDeck

--- GoW/cardDeck.py
+++ GoW/cardDeck.py
@@ -1,16 +1,23 @@
+CardDeck: import random from card import Card
+
 class CardDeck:
+
 	def __init__(self):
 		self.__deck = [None] * 52
 		self.fill()
 		self.__numCards = 52
+
 	def shuffle(self):
 		random.shuffle(self.__deck)
+
 	def deal(self):
 		if(self.__numCards == 0): return None
 		self.__numCards -= 1
 		return self.__deck[self.__numCards]
+
 	def getSize(self):
 		return self.__numCards
+
 	def fill(self):
 		i = 0
 		for r in range(1, 14):
